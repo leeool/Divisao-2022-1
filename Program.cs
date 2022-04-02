2e9e95700301cf6bd0c79d9bc3bdaa7b0bdb1a0a
leeool <lgonsalez02@gmail.com>
2022-04-01 22:37:40 -0300

    finaliza o exercicio

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int deno, nume;
+double  resultado;
+
+Console.Write("\nDigite o numerador: ");
+nume = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o denominador: ");
+deno = Convert.ToInt32(Console.ReadLine());
+
+bool zero = deno == 0;
+
+if (zero)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("\nNão é possivel dividir por 0!");
+    Console.ResetColor();
+}
+else
+{
+    resultado = nume / deno;
+    Console.WriteLine($"\n{nume} dividido por {deno} é igual a {resultado}");
+}
